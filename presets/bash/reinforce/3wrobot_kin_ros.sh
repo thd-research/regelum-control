15d325dcc3fbdcad7b01a18af49a1a394eca018f
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-07-03 14:28:00 +0200

    add new 3wheel limit

--- presets/bash/reinforce/3wrobot_kin_ros.sh
+++ presets/bash/reinforce/3wrobot_kin_ros.sh
@@ -3,9 +3,11 @@ python3.10 run_stable.py \
             --jobs=-1 \
             simulator=ros \
             scenario=reinforce_train \
-            system=3wrobot_kin \
+            initial_conditions=ic_3wrobot_kin \
+            running_objective=quadratic_3wrobot_kin \
+            system=3wrobot_kin_ros \
             --experiment=reinforce_3wrobot_kin \
-            scenario.N_episodes=1 \
+            scenario.N_episodes=4 \
                 scenario.N_iterations=3000 \
             scenario.policy_opt_method_kwargs.lr=0.01 \
             scenario.policy_model.n_hidden_layers=2 \
@@ -15,4 +17,4 @@ python3.10 run_stable.py \
             scenario.policy_model.std=0.15 \
             scenario.policy_model.leaky_relu_slope=0.01 \
             simulator.time_final=15 \
-            system_specific.sampling_time=0.05
+            system_specific.sampling_time=0.1
